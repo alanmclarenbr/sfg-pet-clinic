4e845fd01dad924ea4aa7eb9df49c1c5ccf2993a
alanmclarenbr <alan.ses@gmail.com>
2021-12-01 20:02:39 +0100

    Updated base entity with JPA annotattions

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -3,13 +3,20 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-
     private static final long serialVersionUID = 2441302184792880252L;
-    private Long id;
 
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -11,11 +11,11 @@ public class VetController {
 
     private final VetService vetService;
 
-    public VetController(VetService vetController) {
-        this.vetService = vetController;
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/index", "/index.html"})
+    @RequestMapping({"", "/index", "/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
 
